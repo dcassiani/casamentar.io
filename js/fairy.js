379e79a554b18129ac2b0ca559087019d43ab596
Daniel Cassiani Rezende <daniel.cassiani@anbima.com.br>
2017-01-23 18:28:22 -0200

    firebase base app

--- js/fairy.js
+++ js/fairy.js
@@ -1,6 +1,6 @@
-var mainApp = angular.module('mainApp',  ['ngResource', 'ngRoute']); 
+var casamentarApp = angular.module('casamentarApp',  ['ngResource', 'ngRoute']); 
 
-mainApp.factory('ConvidadoDAO', ['$resource', function($resource) {
+casamentarApp.factory('ConvidadoDAO', ['$resource', function($resource) {
 	return $resource('https://casamentario-dd6e7.firebaseio.com/convidado.json', null,
 		{
 			'confirmar': { method:'PUT' , params:{nome:'Fairy teste'}}
@@ -9,19 +9,22 @@ mainApp.factory('ConvidadoDAO', ['$resource', function($resource) {
 }]);
 
 
-mainApp.controller('mainControl',  ['$scope', '$routeParams', 'ConvidadoDAO',
-                                   function($scope, $routeParams, ConvidadoDAO) {
+casamentarApp.controller('homeControl', //['$scope', '$routeParams', 'ConvidadoDAO',
+                                   function($scope)//, $routeParams, ConvidadoDAO) 
+								   {
 		
-		
+	$scope.teste = 'Angular LOADED';	
 	
 	$scope.confirmarConvidado = function(){
 		var convidado = '{"nome":"Daniel Teste"}';
-		ConvidadoDAO.confirmar(null, convidado);	
+		//ConvidadoDAO.confirmar(null, convidado);	
 		$scope.teste = 'DAO done!';
 	};
 	  
-	$scope.teste = 'Angular LOADED';
+	
 
-}]);
+}
+//]
+);
 
 
