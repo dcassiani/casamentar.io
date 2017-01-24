44d86cdefbd605339d64adccf5b5950770a1a63e
Daniel Cassiani Rezende <daniel.cassiani@anbima.com.br>
2017-01-24 18:26:49 -0200

    http.put to firebase (XHR threat)

--- js/fairy.js
+++ js/fairy.js
@@ -1,30 +1,45 @@
-var casamentarApp = angular.module('casamentarApp',  ['ngResource', 'ngRoute']); 
+var casamentarApp = angular.module('casamentarApp', []); 
 
-casamentarApp.factory('ConvidadoDAO', ['$resource', function($resource) {
-	return $resource('https://casamentario-dd6e7.firebaseio.com/convidado.json', null,
-		{
-			'confirmar': { method:'PUT' , params:{nome:'Fairy teste'}}
-		}
-	);
-}]);
+//casamentarApp.factory('ConvidadoDAO', ['$resource', function($resource) {
+//	return $resource('https://casamentario-dd6e7.firebaseio.com/convidado.json', null,
+//		{
+//			'confirmar': { method:'PUT' , params:{nome:'Fairy teste'}}
+//		}
+//	);
+//}]);
 
 
-casamentarApp.controller('homeControl', //['$scope', '$routeParams', 'ConvidadoDAO',
-                                   function($scope)//, $routeParams, ConvidadoDAO) 
-								   {
+casamentarApp.controller('homeControl',  ['$scope', '$http', function($scope, $http){
 		
 	$scope.teste = 'Angular LOADED';	
 	
 	$scope.confirmarConvidado = function(){
-		var convidado = '{"nome":"Daniel Teste"}';
-		//ConvidadoDAO.confirmar(null, convidado);	
-		$scope.teste = 'DAO done!';
+		
+		var req = {
+			method: 'PUT',
+			url: 'https://casamentario-dd6e7.firebaseio.com/convidado.json',
+			data: {
+					nome: 'Daniel Teste'
+			}
+		}
+		
+		memoriesService = $http(req);
+		memoriesService.then(function(resp){
+			alert('sucesso');
+			//sucesso
+			$scope.teste = resp;			
+		}, function(resp){
+			alert('erro' + resp);
+			//erro
+			$scope.teste = resp;
+		});
+		
+//		var convidado = '{"nome":"Daniel Teste"}';
+//		ConvidadoDAO.confirmar(null, convidado);	
+		$scope.teste = $scope.teste + ' - DAO done!';
 	};
-	  
-	
-
 }
-//]
+]
 );
 
 
